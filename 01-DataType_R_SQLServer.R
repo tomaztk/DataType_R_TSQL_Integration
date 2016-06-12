0c3cfa363b9e395c785913fa669773cbfcb8c72d
tomaztk <tomaz.kastrun@gmail.com>
2016-06-12 22:21:30 +0200

    Add files via upload

---
+++ 01-DataType_R_SQLServer.R
@@ -0,0 +1,52 @@
+#~~~~~~~~~~~~~~~~~~~~~~~
+# Test of special characters using RODBC library
+# Author: Tomaz Kastrun (tomaz.kastrun@gmail.com)
+# Date: June 11, 2016
+#~~~~~~~~~~~~~~~~~~~~~~~
+
+library(RODBC)
+
+myconn <-odbcDriverConnect("driver={SQL Server};Server=SICN-00031\\SQLSERVER2016RC3;database=Adventureworks;trusted_connection=true")
+
+
+cust.data <- sqlQuery(myconn, "SELECT 1 AS Nof, 'D' AS GROUPs
+                     UNION ALL SELECT 2 AS Nof, 'A' AS GROUPs
+                     UNION ALL SELECT 3 AS Nof, 'A' AS GROUPs
+                     UNION ALL SELECT 1 AS Nof, 'D' AS GROUPs
+                     UNION ALL SELECT 3 AS Nof, 'È' AS GROUPs
+                     UNION ALL SELECT 4 AS Nof, 'È' AS GROUPs")
+
+cust.data2 <- sqlQuery(myconn, "SELECT 1 AS Nof, 'D' AS GROUPs
+                      UNION ALL SELECT 2 AS Nof, 'A' AS GROUPs
+                      UNION ALL SELECT 3 AS Nof, 'A' AS GROUPs
+                      UNION ALL SELECT 1 AS Nof, 'D' AS GROUPs
+                      UNION ALL SELECT 3 AS Nof, 'È' AS GROUPs
+                      UNION ALL SELECT 4 AS Nof, 'È' AS GROUPs
+                      UNION ALL SELECT 6 AS Nof, 'C' AS GROUPs")
+close(myconn) 
+
+
+mytable <- with(cust.data, table(Nof,GROUPs))
+df <- data.frame(margin.table(mytable,2))
+df
+
+mytable2 <- with(cust.data2, table(Nof,GROUPs))
+df2 <- data.frame(margin.table(mytable2,2))
+df2
+
+df
+df2
+
+# Without ROBC Library
+# Using only R
+# In this case it functions Natively OK
+cust.data3 <- data.frame(Nof=c(1,2,3,1,3,4,6), GROUPs=c('D','A','A','D','È','È','C'))
+
+mytable3 <- with(cust.data3, table(Nof,GROUPs))
+df3 <- data.frame(margin.table(mytable3,2))
+df3
+
+
+
+
+
